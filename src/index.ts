64c25a873de630975e9c6395e6d648a036033f0c
Wang Xinping <xinping.wang@hotmail.com>
2025-04-09 21:03:08 +0800

    feat: simple bundler from chinanf-boy/minipack-explain

---
+++ src/index.ts
@@ -0,0 +1,98 @@
+import fs from "node:fs";
+import * as parser from "@babel/parser";
+import traverse from "@babel/traverse";
+import { transformFromAstSync } from "@babel/core";
+import path from "node:path";
+
+let ID = 0;
+
+type Asset = {
+  id: number;
+  filename: string;
+  dependencies: string[];
+  code: string | null | undefined;
+  mapping: Record<string, number>;
+};
+
+function createAsset(filename: string): Asset {
+  const content = fs.readFileSync(filename, "utf-8");
+
+  const ast = parser.parse(content, { sourceType: "module" });
+  const dependencies: string[] = [];
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
+    },
+  });
+
+  const id = ID++;
+
+  const { code } =
+    transformFromAstSync(ast, undefined, {
+      presets: ["@babel/env"],
+    }) ?? {};
+
+  return {
+    id,
+    filename,
+    dependencies,
+    code,
+    mapping: {} as Record<string, number>,
+  };
+}
+
+function createGraph(entry: string) {
+  const mainAsset = createAsset(entry);
+
+  const queue = [mainAsset];
+
+  for (const asset of queue) {
+    const dirname = path.dirname(asset.filename);
+
+    asset.dependencies.forEach((relativePath) => {
+      const absolutePath = path.resolve(dirname, relativePath);
+
+      const child = createAsset(absolutePath);
+      asset.mapping[relativePath] = child.id;
+      queue.push(child);
+    });
+  }
+
+  return queue;
+}
+
+function bundle(graph: Asset[]) {
+  let modules = "";
+
+  graph.forEach((mod) => {
+    modules += `${mod.id}: [
+    function (require, module, exports) { ${mod.code} },
+    ${JSON.stringify(mod.mapping)},
+    ],`;
+  });
+
+  const result = `
+    (function(modules) {
+      function require(id) {
+        const [fn, mapping] = modules[id];
+
+        function localRequire(name) {
+          return require(mapping[name]);
+        }
+
+        const module = { exports: {} };
+        fn(localRequire, module, module.exports);
+        return module.exports;
+      }
+
+      require(0);
+    })({${modules}})
+  `;
+
+  return result;
+}
+
+const graph = createGraph(path.resolve(__dirname, "../example/src/entry.js"));
+const result = bundle(graph);
+
+fs.writeFileSync(path.resolve(__dirname, "../example/dist/bundle.js"), result);
